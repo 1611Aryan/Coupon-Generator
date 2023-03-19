263510f561cc63e4dc65364fff7652b2b47fdc6c
aryan-aegis <aryan@aegisconvenant.com>
2023-03-20 00:42:07 +0530

    canvas setup

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,36 @@ import { createCanvas, loadImage } from 'canvas';
 import fs from 'fs';
 
 const width = 1000,
-  height = 600;
+  height = 600,
+  boxWidth = 280,
+  boxHeight = 70;
 const canvas = createCanvas(width, height);
 const ctx = canvas.getContext('2d');
 
 // Write "Awesome!"
-ctx.font = '100px Arial';
-
-ctx.fillText('Awesome!', 50, 100);
-
-// Draw line under text
-var text = ctx.measureText('Awesome!');
-ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-ctx.beginPath();
-ctx.lineTo(50, 102);
-ctx.lineTo(50 + text.width, 102);
-ctx.stroke();
 
 // Draw cat with lime helmet
-loadImage('dist/coupon/coupon.png').then(image => {
+loadImage('dist/static/coupon/coupon.png').then(image => {
   ctx.drawImage(image, 0, 0, width, height);
-  fs.writeFileSync('./image.png', canvas.toBuffer());
+
+  ctx.fillStyle = '#2e75b6';
+  ctx.fillRect(
+    width - width / 20 - boxWidth,
+    height / 2.45,
+    boxWidth,
+    boxHeight
+  );
+  ctx.font = '40px Impact bold';
+  ctx.fillStyle = '#ffff00';
+  const text = ctx.measureText('TEST0001');
+  ctx.fillText('TEST0001', width - text.width - width / 10, height / 2.05);
+
+  // Draw line under text
+  // const text = ctx.measureText('Awesome!');
+  // ctx.strokeStyle = 'ffff00';
+
+  loadImage('dist/static/logos/orion.png').then(img => {
+    ctx.drawImage(img, width / 10, 0, width / 10, width / 10);
+    fs.writeFileSync('gen/image.png', canvas.toBuffer());
+  });
 });
